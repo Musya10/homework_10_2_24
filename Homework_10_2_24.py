44571b292901ae58272dfcba7ed16e44f54f7927
Vitaliy <vitaliymusienko10@gmail.com>
2024-02-13 20:53:49 +0200

    Correct first features

--- Homework_10_2_24.py
+++ Homework_10_2_24.py
@@ -1,14 +1,15 @@
 """Task1
 Користувач вводить три цифри з клавіатури. Залежно від вибору користувача програма виводить
 на екран максимум з трьох, мінімум іх трьох або середноарифметичне трьох чисел"""
-c1 = int(input("Choice what you want "
-               "Enter 1 Maximum of three numbers. "
-               "Enter 2 Minimum of three numbers. "
-               "Enter 3 Arithmetic mean of thee numbers: "))
 
 n1 = int(input("Enter first digit number"))
 n2 = int(input("Enter second digit number"))
 n3 = int(input("Enter third digit number"))
+
+c1 = int(input("Choice what you want "
+               "Enter 1 Maximum of three numbers. "
+               "Enter 2 Minimum of three numbers. "
+               "Enter 3 Arithmetic mean of thee numbers: "))
 
 if 0 < c1 < 2:
     print(f"Maximum: {max(n1, n2, n3)}")
